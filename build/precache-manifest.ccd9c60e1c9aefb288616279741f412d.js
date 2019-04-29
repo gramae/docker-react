780e6aa71f1fb81350fabecf666c4cb732213e89
egrama <egrama@EGRAMA-RO.ro.oracle.com>
2019-04-29 20:06:50 +0300

    adding initial work

---
+++ build/precache-manifest.ccd9c60e1c9aefb288616279741f412d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5296f427e0432728160",
+    "url": "/static/js/main.cf235baa.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b5296f427e0432728160",
+    "url": "/static/css/main.23419617.chunk.css"
+  },
+  {
+    "revision": "249ccdefe3eca07028046203cc3f35cc",
+    "url": "/index.html"
+  }
+];
